02a80255706e3fe6f73ba8fd22fb10659b52018d
Irina Baron <irr.baron@gmail.com>
2022-03-25 22:13:16 +0200

    added files, worked with index

---
+++ gulpfile.js
@@ -0,0 +1,191 @@
+'use strict';
+// import { src, dest, series, watch, parallel } from 'gulp';
+
+
+
+import gulp from 'gulp';
+const { series, parallel } = pkg;
+import pkg from 'gulp';
+const { src, dest } = pkg1;
+import pkg1 from 'gulp';
+import concat from 'gulp-concat';
+
+import fileinclude from 'gulp-file-include';
+import htmlMin from 'gulp-htmlmin';
+import autoprefixes from 'gulp-autoprefixer';
+import cleanCSS from 'gulp-clean-css';
+import svgSprite from 'gulp-svg-sprite';
+import image from 'gulp-image';
+import babel from 'gulp-babel';
+import uglify from 'gulp-uglify-es';
+import { onError } from 'gulp-notify';
+const { init, write } = pkg2;
+import pkg2 from 'gulp-sourcemaps';
+import del from 'del';
+import browserSync from 'browser-sync';
+import pkg3 from 'node-notifier';
+const { notify } = pkg3;
+import dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+const sass = gulpSass(dartSass);
+
+import watch from 'gulp-watch';
+browserSync.create();
+// const scss = require('gulp-sass')(require('sass'));
+// const browserSync = require('browser-sync').create();
+
+const clean = () => {
+  return del(['dist'])
+}
+
+const resources = () => {
+  return src('src/resources/**')
+
+}
+
+const styles = async () => {
+  return src(['src/styles/normalize.css','src/styles/style.scss','src/styles/*.scss'])
+    .pipe(pkg2.init())
+    .pipe(pkg2.write())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(concat('main.css'))
+    .pipe(dest('dist/styles'))
+    .pipe(browserSync.stream())
+
+}
+
+
+const html = async () => {
+  return src('src/**/*.html')
+  .pipe(fileinclude())
+  .pipe(dest('dist'))
+  .pipe(browserSync.stream())
+}
+
+const htmlMinify = async () => {
+  return src('src/**/*.html')
+    .pipe(htmlMin({
+      collapseWhitespace: true,
+    }))
+    .pipe(dest('dist'))
+    .pipe(browserSync.stream())
+
+}
+
+const svgSprites = async () => {
+  return src('src/images/svg/**/*.svg')
+    .pipe(svgSprite({
+      mode: {
+        stack: {
+          sprite: '../sprite.svg'
+        }
+      }
+    }))
+}
+
+const scripts = async () => {
+  return src([
+    'src/js/**/*.js',
+    'src/js/main.js'
+  ])
+    .pipe(pkg2.init())
+    .pipe(babel({
+      presets: ['@babel/env']
+    }))
+    // .pipe(uglify().on('error', onError()))
+    .pipe(write())
+    .pipe(concat('app.js'))
+    .pipe(dest('dist/js'))
+    .pipe(browserSync.stream())
+}
+
+const fonts = () => {
+  return src(['src/fonts/**/*.woff', 'src/fonts/**/*.woff2'])
+  .pipe(dest('dist/fonts'))
+}
+
+const images = () => {
+  return src([
+    'src/images/**/*.jpg',
+    'src/images/**/*.jpeg',
+    'src/images/**/*.png',
+    'src/images/**/*.webp',
+    'src/images/**/*.svg',
+  ])
+    .pipe(image())
+    .pipe(dest('dist/images'))
+    .pipe(browserSync.stream())
+}
+
+const prebuild = async function () {
+  const ind = src('src/**/*.html')
+    .pipe(fileinclude())
+    .pipe(dest('dist'))
+  const resource = src('src/resources/**')
+  //   .pipe(dest('dist'))
+  const css = src(['src/styles/normalize.css','src/styles/**/*.scss'])
+  .pipe(sass().on('error', sass.logError))
+    .pipe(concat('main.css'))
+    .pipe(autoprefixes({
+      cascade: false
+    }))
+    .pipe(cleanCSS({
+      level: 2
+    }))
+    .pipe(dest('dist/styles'))
+  
+}
+
+const buildM = function () {
+  const js = src([
+    'src/js/**/*.js',
+    'src/js/main.js'
+  ])
+    .pipe(concat('app.js'))
+    .pipe(dest('dist/js'))
+  const spritesSvg = src('src/images/svg/**/*.svg')
+    .pipe(dest('dist/images'))
+  const fnt = src(['src/fonts/**/*.woff', 'src/fonts/**/*.woff2'])
+    .pipe(dest('dist/fonts'))
+    // return src(['src/fonts/**/*.woff'])
+    // .pipe(dest('dist/fonts'))
+}
+
+const watchFiles = async () => {
+  watch('src/styles/**/*.scss', ['sass'])
+  watch('src/styles/**/*.css', styles)
+  watch('src/**/*.html', html)
+  watch('src/**/*.html', htmlMinify)
+  watch([
+    'src/images/**/*.jpg',
+    'src/images/**/*.jpeg',
+    'src/images/**/*.png',
+    'src/images/*.svg',
+  ], images)
+  watch('src/images/svg/**/*.svg', svgSprites)
+  watch('src/js/**/*.js', scripts)
+  watch('src/resources/**', resources)
+  browserSync.init({
+    server: {
+      baseDir: './dist/'
+    },
+    notify: false
+  })
+}
+
+// watch('src/styles/**/*.css', styles)
+// watch('src/styles/**/*.scss', styles)
+// watch('src/**/*.html', htmlMinify)
+// watch('src/**/*.html', html)
+// watch([
+//   'src/images/**/*.jpg',
+//   'src/images/**/*.jpeg',
+//   'src/images/**/*.png',
+//   'src/images/*.svg',
+// ], images)
+// watch('src/images/svg/**/*.svg', svgSprites)
+// watch('src/js/**/*.js', scripts)
+// watch('src/resources/**', resources)
+
+export const dev = series(resources, parallel(styles, scripts, html, fonts), svgSprites, images, watchFiles)
+export const build = series(clean, htmlMinify, fonts, images, parallel(prebuild, buildM))
---
+++ src/js/main.js
@@ -0,0 +1,149 @@
+document.addEventListener('DOMContentLoaded', () => {
+  try {
+    // burger-menu 
+
+    const burgerBtn = document.querySelector('.header__burger')
+
+    burgerBtn.addEventListener('click', function () {
+      burgerBtn.classList.toggle('active');
+      document.querySelector('.header__nav').classList.toggle('active');
+      document.querySelector('.contacts__phone').classList.toggle('active')
+      // document.querySelector('body').classList.toggle('lock');
+    })
+
+    const searchBtn = document.querySelector('.search__btn');
+
+    searchBtn.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector('.header__search').classList.toggle('active');
+      document.querySelector('body').classList.toggle('lock');
+    })
+
+    const searchClose = document.querySelector('.search__span');
+    searchClose.addEventListener('click', function () {
+      document.querySelector('.header__search').classList.remove('active')
+      document.querySelector('body').classList.toggle('lock');
+    })
+    // about
+
+    // form__map
+    let closeInfo = document.querySelector('.contacts__close');
+
+    closeInfo.addEventListener('click', function () {
+      document.querySelector('.contacts__info.active').classList.remove('active');
+    })
+
+    //form__input
+
+    const nameDiv = document.querySelector('.contacts__form-text');
+    const emailDiv = document.querySelector('.contacts__form-email');
+
+    let input = document.querySelector('.contacts__input');
+    let nameInput = document.querySelector('.name');
+    let emailInput = document.querySelector('.email');
+    let btn = document.querySelector('.contacts__form-btn');
+    let aboutBtn = document.querySelector('.form__btn');
+    let aboutInput = document.querySelector('.form__text');
+
+    let mailformat = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+    let simbol = /^[а-яА-ЯёЁ\s]+$/;
+    let div = document.createElement('div');
+    let div1 = document.createElement('div');
+    let div2 = document.createElement('div');
+
+
+    btn.addEventListener('click', function (e) {
+      e.preventDefault();
+      let validName = validateName();
+      let validEmail = validateEmail();
+      // if(!validName && !validEmail) {
+      //   return
+      // }
+    })
+
+    aboutBtn.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      div2.classList.add('validate');
+      document.querySelector('.about__form').append(div2);
+      if (aboutInput.value == '') {
+        div2.innerHTML = 'Введите email';
+        aboutInput.style.borderColor = '#F06666';
+      } else if (aboutInput.value.match(mailformat)) {
+        div2.innerHTML = '';
+        aboutInput.style.borderColor = 'transparent';
+        // return true
+      } else if (!aboutInput.value.match(mailformat)) {
+        div2.innerHTML = 'Недопустимый формат';
+        aboutInput.style.borderColor = '#F06666';
+      }
+    })
+
+    function validateName() {
+      div.classList.add('validate');
+      nameDiv.append(div);
+      if (nameInput.value == '') {
+        div.innerHTML = 'Введите имя';
+        nameInput.style.borderColor = '#FF3030';
+      } else if (!nameInput.value.match(simbol)) {
+        div.innerHTML = 'Недопустимый формат';
+        nameInput.style.borderColor = '#FF3030';
+      } else if (nameInput.value.match(simbol)) {
+        div.innerHTML = '';
+        nameInput.style.borderColor = 'transparent';
+        return true
+      }
+
+    }
+
+    function validateEmail() {
+      div1.classList.add('validate');
+      emailDiv.append(div1);
+      if (emailInput.value == '') {
+        div1.innerHTML = 'Введите email';
+        emailInput.style.borderColor = '#FF3030';
+      } else if (emailInput.value.match(mailformat)) {
+        div1.innerHTML = '';
+        emailInput.style.borderColor = 'transparent';
+        return true
+      } else if (!emailInput.value.match(mailformat)) {
+        div1.innerHTML = 'Недопустимый формат';
+        emailInput.style.borderColor = '#FF3030';
+
+      }
+
+    }
+
+    // ------------scroll--------------
+    const btnScroll = document.querySelector('.btn__scroll');
+    window.addEventListener('scroll', () => {
+      let scrollNum = window.pageYOffset;
+      document.querySelector('.footer__go');
+      document.querySelector('.footer__link_logo');
+      if (scrollNum >= '100') {
+        btnScroll.style.display = 'block';
+        btnScroll.style.position = 'fixed';
+        btnScroll.style.bottom = '80px';
+        btnScroll.style.right = '40px';
+
+      } else {
+        btnScroll.style.display = 'none';
+      }
+
+    }, { passive: true })
+
+    let links = document.querySelectorAll('.hero__link');
+
+    links.forEach(link => {
+      link.addEventListener('click', () => {
+        window.scroll({ behavior: 'smooth' })
+      })
+    })
+
+
+  } catch (error) {
+    console.log(error)
+  }
+
+
+})
