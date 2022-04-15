c873f09d6464e4d9b78056537bb5eddbdf734f1d
Александр Варламов <alexandralexandrovich007@gmail.com>
2022-04-15 21:08:51 +0300

    worked with form page contacts

---
+++ src/js/contacts.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', () => {
+  try {
+    let inputs = document.querySelectorAll('.contacts-hero__input');
+    console.log(inputs.length)
+
+    // inputs.forEach(input => {
+    //   input.addEventListener('click', () => {
+    //     // if(input.value == '') {
+    //       input.style.borderBottom = '2px solid #393246';
+    //     // }
+        
+    //   })
+    // })
+
+
+
+  } catch (error) {
+    console.log(error);
+  }
+
+
+
+
+})
